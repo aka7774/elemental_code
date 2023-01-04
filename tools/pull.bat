99f2c97c51fa043dd625f937814ac7a3e3eef0cc
aka7774 <aka7774@users.noreply.github.com>
2023-01-05 04:09:51 +0900

    Update Installer

--- tools/pull.bat
+++
@@ -1,3 +0,0 @@
-cd H:\Stable Diffusion\elemental_code\tools\stable-diffusion-webui 
-H:\Stable Diffusion\elemental_code\tools\PortableGit\bin\git pull 
-@pause 
--- tools/start.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-set PATH=;H:\Stable Diffusion\elemental_code\tools\\python310\Scripts;H:\Stable Diffusion\elemental_code\tools\PortableGit\bin 
-set PYTHON=H:\Stable Diffusion\elemental_code\tools\python310\python.exe 
-set GIT=H:\Stable Diffusion\elemental_code\tools\PortableGit\bin\git.exe 
-set VENV_DIR= 
-set COMMANDLINE_ARGS=--xformers 
-cd H:\Stable Diffusion\elemental_code\tools\stable-diffusion-webui 
-call webui.bat 
