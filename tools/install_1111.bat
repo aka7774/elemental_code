6a706f6f358569c2786616b877111260cd04660b
aka7774 <aka7774@users.noreply.github.com>
2023-01-05 06:29:08 +0900

    Update Installer

--- tools/install_1111.bat
+++ tools/install_1111.bat
@@ -5,28 +5,36 @@ rem 環境を汚さない(PythonとGitへのPATHも通さない)仕様
 rem インストールしたいディレクトリを作ってその中にこのファイルを入れて実行する
 set INSTALL_DIR=%~dp0
 cd /d %INSTALL_DIR%
+mkdir dl
 
 rem bitsadminはwget的なもの。標準搭載。便利だけどなんか遅い。
-bitsadmin /transfer Python https://www.python.org/ftp/python/3.10.9/python-3.10.9-embed-amd64.zip %INSTALL_DIR%python-3.10.9-embed-amd64.zip
+bitsadmin /transfer python https://www.python.org/ftp/python/3.10.9/python-3.10.9-embed-amd64.zip %INSTALL_DIR%dl\python-3.10.9-embed-amd64.zip
 rem unzipコマンドは標準で存在しないらしい
-call powershell -command "Expand-Archive -Force python-3.10.9-embed-amd64.zip"
+call powershell -command "Expand-Archive -Force dl\python-3.10.9-embed-amd64.zip"
 ren python-3.10.9-embed-amd64 python310
 
-rem カレントディレクトリにpathを通す
-echo import sys; sys.path.append(''); > python310\current.pth
+rem python用のpathを通す
+echo import sys; sys.path.append('')>python310\current.pth
+echo ./Lib/site-packages>>python310\python310._pth
 
 rem pipインストール
 set PYTHON=%INSTALL_DIR%python310\python.exe
 set PATH=%PATH%;C:\SD2\python310\Scripts
-bitsadmin /transfer pip https://bootstrap.pypa.io/get-pip.py %INSTALL_DIR%get-pip.py
-%PYTHON% %INSTALL_DIR%get-pip.py
+bitsadmin /transfer pip https://bootstrap.pypa.io/get-pip.py %INSTALL_DIR%dl\get-pip.py
+%PYTHON% %INSTALL_DIR%dl\get-pip.py
 
 rem venvインストール
-%PYTHON% -m pip install virtualenv
+bitsadmin /transfer venv https://raw.githubusercontent.com/aka7774/elemental_code/main/tools/venv.zip %INSTALL_DIR%dl\venv.zip
+call powershell -command "Expand-Archive -Force dl\venv.zip -DestinationPath python310\Lib\site-packages"
+
+rem ensurepipインストール
+bitsadmin /transfer ensurepip https://raw.githubusercontent.com/aka7774/elemental_code/main/tools/ensurepip.zip %INSTALL_DIR%dl\ensurepip.zip
+call powershell -command "Expand-Archive -Force dl\ensurepip.zip -DestinationPath python310\Lib\site-packages"
 
 rem gitインストール
 bitsadmin /transfer git https://github.com/git-for-windows/git/releases/download/v2.39.0.windows.2/PortableGit-2.39.0.2-64-bit.7z.exe %INSTALL_DIR%PortableGit-2.39.0.2-64-bit.7z.exe
 %INSTALL_DIR%PortableGit-2.39.0.2-64-bit.7z.exe -y
+move PortableGit-2.39.0.2-64-bit.7z.exe dl\
 
 set GIT=%INSTALL_DIR%PortableGit\bin\git
 %GIT% clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
